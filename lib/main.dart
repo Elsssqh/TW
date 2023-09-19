f36330ee7302f643eb4a860f3ac026a7c5d76d62
Elsshqh <els.hqh@gmail.com>
2023-09-19 15:47:45 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+// import 'package:app/Pagelist/directM.dart';
+// import 'package:flutter/foundation.dart';
+// import 'Pagelist/Homepage.dart';
+// import 'Pagelist/chatpage.dart';
+// import 'Pagelist/cover.dart';
+// import 'Pagelist/followingpage.dart';
+// import 'Pagelist/loginpage.dart';
+// import 'Pagelist/profilepage.dart';
+// import 'Pagelist/signup.dart';
+// import 'Pagelist/trendingpage.dart';
+// import 'package:flutter/material.dart';
+
+// class APP extends StatelessWidget {
+//   const APP({Key? key}): super(key:key);
+
+//   @override
+//     Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Flutter Auth',
+//       theme: ThemeData(
+//         primaryColor: Colors.white,
+//         scaffoldBackgroundColor: Colors.white,
+//       ),
+//       initialRoute: '/',
+//       routes: {
+//         '/cover': (context) => cover(),
+//         '/homepage': (context) =>  Homepage(),
+//         '/loginpage': (context) => LoginPage(),
+//         '/signup': (context) =>  SignUp(),
+//         '/profile': (context) =>   ProfilePage(),
+//         '/followingpage': (context) =>  FollowingProfile(),
+//         '/trendingpage': (context) =>  Trendingpage(),
+//         '/DMpage':(context)=> Chat(),
+//         '/chatpage':(context)=> Chatpage()
+//       },
+//     );
+//   }
+// }
+
+// class SecondScreen extends StatelessWidget {
+//   const SecondScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Flutter Auth',
+//       theme: ThemeData(
+//         primaryColor: Colors.white,
+//         scaffoldBackgroundColor: Colors.white,
+//       ),
+//       initialRoute: '/',
+//       routes: {
+//         '/cover': (context) => cover(),
+//         '/homepage': (context) =>  Homepage(),
+//         '/loginpage': (context) => LoginPage(),
+//         '/signup': (context) =>  SignUp(),
+//         '/profile': (context) =>   ProfilePage(),
+//         '/followingpage': (context) =>  FollowingProfile(),
+//         '/trendingpage': (context) =>  Trendingpage(),
+//         '/DMpage':(context)=> Chat(),
+//         '/chatpage':(context)=> Chatpage()
+//       },
+//     );
+//   }
+// }
